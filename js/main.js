332292a0e5f29651128b7547193a0de518366964
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-02-11 03:03:10 +0100

    feat: axios get api

--- js/main.js
+++ js/main.js
@@ -5,7 +5,22 @@ createApp({
     return {
         
     }
-  }
+  },
+
+  methods: {
+    
+  },
+
+  created() {
+    
+    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((response) => {
+
+            console.log(response.data.response)
+
+        })
+    },
+
 }).mount('#app')
 
 
